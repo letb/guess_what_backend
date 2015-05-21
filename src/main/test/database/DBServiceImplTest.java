f40d2053a8bd639dfa507ba2ab292a35568fc4b6
Ivan Linnik <ivan_linnik@mail.ru>
2015-05-21 20:32:05 +0300

    Add tests for DB

---
+++ src/main/test/database/DBServiceImplTest.java
@@ -0,0 +1,52 @@
+package database;
+
+import base.DBService;
+import base.dataSets.UserDataSet;
+import dbService.DBServiceImpl;
+import junit.framework.Assert;
+import org.hibernate.cfg.Configuration;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class DBServiceImplTest {
+    DBService dbService;
+
+    @Before
+    public void configurateDB() throws Exception {
+        Configuration configuration = new Configuration();
+
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
+        configuration.setProperty("hibernate.connection.url", "jdbc:h2:mem:java_blackjack_test");
+        configuration.setProperty("hibernate.connection.username", "");
+        configuration.setProperty("hibernate.connection.password", "");
+        configuration.setProperty("hibernate.show_sql", "true");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "create");
+
+        dbService = new DBServiceImpl(configuration);
+    }
+
+    @Test
+    public void saveUserAndGetHimTest() {
+        UserDataSet user = new UserDataSet("first","123","first@first");
+        dbService.save(user);
+        UserDataSet userFromDB = dbService.readByLogin("first");
+        assertEquals(user.getJson(), userFromDB.getJson());
+    }
+
+    @Test
+    public void getScoreboardTest() {
+        UserDataSet first = new UserDataSet("first", "1", "first@mail");
+        UserDataSet second = new UserDataSet("second", "1", "second@mail");
+        UserDataSet third = new UserDataSet("third", "1", "third@mail");
+        dbService.save(first);
+        dbService.save(second);
+        dbService.save(third);
+        List users = dbService.getScoreboard();
+        assertEquals(3, users.size());
+    }
+}
