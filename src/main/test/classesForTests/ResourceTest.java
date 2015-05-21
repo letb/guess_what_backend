d2e4a4baaea2ba0aca69051bab06e9e54ca8d9e1
Ivan Linnik <ivan_linnik@mail.ru>
2015-05-21 18:32:07 +0300

    Add test for Resource System

---
+++ src/main/test/classesForTests/ResourceTest.java
@@ -0,0 +1,16 @@
+package classesForTests;
+
+public class ResourceTest {
+    int intField;
+    String strField;
+
+    public ResourceTest() {}
+
+    public int getIntField() {
+        return intField;
+    }
+
+    public String getStrField() {
+        return strField;
+    }
+}
